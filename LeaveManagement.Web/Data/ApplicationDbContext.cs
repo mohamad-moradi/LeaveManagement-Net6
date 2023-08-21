497cdc0adedd1321b370107a3d45ee6302d28c9f
MHDMURADI <mohamadmoradi472@gmail.com>
2023-08-21 15:55:38 +0300

    Revert "Added Database tables and Migrations,End Implement Database Section."
    This reverts commit 0567cf3508d89533541cb9ffd3b607cdc776ad47.

--- LeaveManagement.Web/Data/ApplicationDbContext.cs
+++ LeaveManagement.Web/Data/ApplicationDbContext.cs
@@ -3,15 +3,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagement.Web.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<Employee>
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
-
-        public DbSet<LeaveType> LeaveTypes { get; set; }
-
-        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
     }
 }
