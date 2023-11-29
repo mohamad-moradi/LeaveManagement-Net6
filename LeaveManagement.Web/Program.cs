427a5171f62951f95650e22c007c7112341203f4
MHDMURADI <mohamadmoradi472@gmail.com>
2023-11-29 23:36:18 +0300

    modified the crud screens and added datatables and swal. End of leave Type Managment Module.

--- LeaveManagement.Web/Program.cs
+++ LeaveManagement.Web/Program.cs
@@ -1,3 +1,4 @@
+using LeaveManagement.Web.Configurations;
 using LeaveManagement.Web.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -18,6 +19,9 @@ namespace LeaveManagement.Web
 
             builder.Services.AddDefaultIdentity<Employee>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            builder.Services.AddAutoMapper(typeof(MapperConfig));
+
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
